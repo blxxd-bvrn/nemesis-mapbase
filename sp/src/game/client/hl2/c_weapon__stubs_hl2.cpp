b96188e9f7d5cde270804bb1199395a4ba3d80d5
blxxd-bvrn <74259725+blxxd-bvrn@users.noreply.github.com>
2024-10-26 10:44:13 +0300

    Adding a sniper but failing

--- sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
+++ sp/src/game/client/hl2/c_weapon__stubs_hl2.cpp
@@ -33,6 +33,7 @@ STUB_WEAPON_CLASS( weapon_smg1, WeaponSMG1, C_HLMachineGun);
 STUB_WEAPON_CLASS( weapon_mp5, WeaponMP5, C_HLSelectFireMachineGun);
 STUB_WEAPON_CLASS( weapon_357, Weapon357, C_BaseHLCombatWeapon );
 STUB_WEAPON_CLASS( weapon_crossbow, WeaponCrossbow, C_BaseHLCombatWeapon );
+STUB_WEAPON_CLASS (weapon_sniperrifle, WeaponSniperRifle, C_BaseHLCombatWeapon);
 
 #ifndef MAPBASE
 STUB_WEAPON_CLASS( weapon_slam, Weapon_SLAM, C_BaseHLCombatWeapon );
